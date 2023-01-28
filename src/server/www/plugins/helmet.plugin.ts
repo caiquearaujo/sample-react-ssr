0874a5ef3f4198bb8874b2b93a801e350b1a72de
Caique Araujo <caique@studiopiggly.com.br>
2023-01-28 18:55:07 -0300

    :bug: fix(server): Allow CORS on development env

--- src/server/www/plugins/helmet.plugin.ts
+++ src/server/www/plugins/helmet.plugin.ts
@@ -1,8 +1,18 @@
 import fastifyHelmet from '@fastify/helmet';
+import { NODE_ENV } from '@server/env/config';
 import { IFastifyInstance, TFnApplyToFastify } from '@server/types';
 
 const callable: TFnApplyToFastify = async (app: IFastifyInstance) => {
-	await app.register(fastifyHelmet);
+	const config =
+		NODE_ENV !== 'production'
+			? {
+					contentSecurityPolicy: false,
+					crossOriginOpenerPolicy: false,
+					originAgentCluster: false,
+			  }
+			: {};
+
+	await app.register(fastifyHelmet, config);
 };
 
 export default callable;
--- src/server/www/plugins/index.ts
+++ src/server/www/plugins/index.ts
@@ -1,14 +1,6 @@
-import { NODE_ENV } from '@server/env/config';
-
 import helmetPlugin from './helmet.plugin';
 import corsPlugin from './cors.plugin';
 import compressPlugin from './compress.plugin';
 import staticPlugin from './static.plugin';
 
-const plugins = [corsPlugin, compressPlugin, staticPlugin];
-
-if (NODE_ENV === 'production') {
-	plugins.push(helmetPlugin);
-}
-
-export default plugins;
+export default [helmetPlugin, corsPlugin, compressPlugin, staticPlugin];
